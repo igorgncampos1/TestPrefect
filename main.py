0cac7d6c5fedc65f9d083a943f0e3260b49b7944
Igor Campos <igorgncampos@gmail.com>
2026-06-29 22:53:01 -0300

    add prefect yaml

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from prefect import flow, task
 from datetime import datetime
 
-@flow(name="hello-flow-prod", flow_run_name=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+@flow(name="TestPrefect", flow_run_name=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"), log_prints=True)
 def hello():
     print("Hello, Prefect!")
 
